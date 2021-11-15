fbd7aecee1d7bc05504321b3f875afd36c1d7cc8
Pavel Sokolov <pavel@tvip.ru>
2021-11-15 19:57:57 +0300

    Split union container and inner messages

--- test/common/union_container_no_union.hpp
+++ test/common/union_container_no_union.hpp
@@ -1,87 +1,9 @@
 #pragma once
 
 #include "union_message.pb.h"
+#include "union_inner_messages.hpp"
 
 using namespace NanoPb::Converter;
-
-struct LOCAL_InnerMessage {
-    enum class Type {
-        UnionInnerOne,
-        UnionInnerTwo,
-        UnionInnerThree
-    };
-    virtual ~LOCAL_InnerMessage(){}
-    virtual Type getType() const = 0;
-
-    template<class T>
-    T* as(){ return static_cast<T*>(this); }
-    template<class T>
-    const T* as() const { return static_cast<const T*>(this); }
-
-    bool operator==(const LOCAL_InnerMessage &rhs) const {
-        return rhs.getType() == getType();
-    }
-
-    bool operator!=(const LOCAL_InnerMessage &rhs) const {
-        return !(rhs == *this);
-    }
-};
-
-struct LOCAL_UnionInnerOne : public LOCAL_InnerMessage {
-    int number = 0;
-
-    LOCAL_UnionInnerOne() = default;
-    LOCAL_UnionInnerOne(int number) : number(number) {}
-
-    Type getType() const override { return Type::UnionInnerOne; }
-
-    bool operator==(const LOCAL_UnionInnerOne &rhs) const {
-        return static_cast<const LOCAL_InnerMessage &>(*this) == static_cast<const LOCAL_InnerMessage &>(rhs) &&
-               number == rhs.number;
-    }
-
-    bool operator!=(const LOCAL_UnionInnerOne &rhs) const {
-        return !(rhs == *this);
-    }
-};
-
-struct LOCAL_UnionInnerTwo : public LOCAL_InnerMessage {
-    std::string str;
-
-    LOCAL_UnionInnerTwo() = default;
-    LOCAL_UnionInnerTwo(const std::string &str) : str(str) {}
-
-    Type getType() const override { return Type::UnionInnerTwo; }
-
-    bool operator==(const LOCAL_UnionInnerTwo &rhs) const {
-        return static_cast<const LOCAL_InnerMessage &>(*this) == static_cast<const LOCAL_InnerMessage &>(rhs) &&
-               str == rhs.str;
-    }
-
-    bool operator!=(const LOCAL_UnionInnerTwo &rhs) const {
-        return !(rhs == *this);
-    }
-};
-
-struct LOCAL_UnionInnerThree : public LOCAL_InnerMessage {
-    using ValuesContainer = std::vector<uint32_t>;
-    ValuesContainer values;
-
-    LOCAL_UnionInnerThree() = default;
-    LOCAL_UnionInnerThree(const ValuesContainer &values) : values(values) {}
-
-    Type getType() const override { return Type::UnionInnerThree; }
-
-    bool operator==(const LOCAL_UnionInnerThree &rhs) const {
-        return static_cast<const LOCAL_InnerMessage &>(*this) == static_cast<const LOCAL_InnerMessage &>(rhs) &&
-               values == rhs.values;
-    }
-
-    bool operator!=(const LOCAL_UnionInnerThree &rhs) const {
-        return !(rhs == *this);
-    }
-};
-
 
 struct LOCAL_UnionContainer {
     std::unique_ptr<LOCAL_InnerMessage> message;
@@ -127,80 +49,6 @@ struct LOCAL_UnionContainer {
                 new LOCAL_UnionInnerThree({0, 1, 2, 3, UINT32_MAX})
         )));
         return ret;
-    }
-};
-
-/******************************************************************************************/
-
-class UnionInnerOneConverter : public AbstractMessageConverter<
-        UnionInnerOneConverter,
-        LOCAL_UnionInnerOne,
-        PROTO_UnionInnerOne,
-        &PROTO_UnionInnerOne_msg>
-{
-public:
-    static ProtoType encoderInit(const EncoderContext& ctx) {
-        return ProtoType {
-                .number = ctx.local.number
-        };
-    }
-
-    static ProtoType decoderInit(DecoderContext& ctx){
-        return ProtoType{
-        };
-    }
-
-    static bool decoderApply(const ProtoType& proto, DecoderContext& ctx){
-        ctx.local.number = proto.number;
-        return true;
-    }
-};
-
-class UnionInnerTwoConverter : public AbstractMessageConverter<
-        UnionInnerTwoConverter,
-        LOCAL_UnionInnerTwo,
-        PROTO_UnionInnerTwo,
-        &PROTO_UnionInnerTwo_msg>
-{
-public:
-    static ProtoType encoderInit(const EncoderContext& ctx) {
-        return ProtoType {
-                .str = StringConverter::encoder(ctx.local.str)
-        };
-    }
-
-    static ProtoType decoderInit(DecoderContext& ctx){
-        return ProtoType{
-                .str = StringConverter::decoder(ctx.local.str)
-        };
-    }
-
-    static bool decoderApply(const ProtoType& proto, DecoderContext& ctx){
-        return true;
-    }
-};
-
-class UnionInnerThreeConverter : public AbstractMessageConverter<
-        UnionInnerThreeConverter,
-        LOCAL_UnionInnerThree,
-        PROTO_UnionInnerThree,
-        &PROTO_UnionInnerThree_msg>
-{
-public:
-    static ProtoType encoderInit(const EncoderContext& ctx) {
-        return ProtoType {
-                .values = ArrayUnsignedConverter<LOCAL_UnionInnerThree::ValuesContainer>::encoder(ctx.local.values)
-        };
-    }
-
-    static ProtoType decoderInit(DecoderContext& ctx){
-        return ProtoType{
-                .values = ArrayUnsignedConverter<LOCAL_UnionInnerThree::ValuesContainer>::decoder(ctx.local.values)
-        };
-    }
-
-    static bool decoderApply(const ProtoType& proto, DecoderContext& ctx){
-        return true;
     }
 };
 
