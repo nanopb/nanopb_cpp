054b7cb3e40c3d3d2b10e921c60f8c078ce83ad4
Pavel Sokolov <pavel@tvip.ru>
2021-12-05 23:20:16 +0300

    Move simple_enum from array to common

---
+++ test/common/simple_enum.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "simple_enum.pb.h"
+
+using namespace NanoPb::Converter;
+
+enum class SimpleEnum {
+    Invalid = 0,
+    ValueOne = 1,
+    ValueTwo = 2
+};
+
+
+class SimpleEnumConverter: public EnumConverter<SimpleEnumConverter, SimpleEnum, PROTO_SimpleEnum> {
+public:
+    static ProtoType encode(const LocalType& local){
+        switch (local) {
+            case SimpleEnum::Invalid:
+                return PROTO_SimpleEnum_Invalid;
+            case SimpleEnum::ValueOne:
+                return PROTO_SimpleEnum_ValueOne;
+            case SimpleEnum::ValueTwo:
+                return PROTO_SimpleEnum_ValueTwo;
+        }
+        return PROTO_SimpleEnum_Invalid;
+    };
+    static LocalType decode(const ProtoType& proto){
+        switch (proto) {
+            case PROTO_SimpleEnum_Invalid:
+                return SimpleEnum::Invalid;
+            case PROTO_SimpleEnum_ValueOne:
+                return SimpleEnum::ValueOne;
+            case PROTO_SimpleEnum_ValueTwo:
+                return SimpleEnum::ValueTwo;
+        }
+        return SimpleEnum::Invalid;
+    };
+};
--- test/tests/array/array.cpp
+++ test/tests/array/array.cpp
@@ -4,42 +4,10 @@
 #include <list>
 
 #include "tests_common.h"
+#include "simple_enum.hpp"
 #include "array.pb.h"
 
 using namespace NanoPb::Converter;
-
-enum class SimpleEnum {
-    Invalid = 0,
-    ValueOne = 1,
-    ValueTwo = 2
-};
-
-
-class SimpleEnumConverter: public NanoPb::Converter::EnumConverter<SimpleEnumConverter, SimpleEnum, PROTO_SimpleEnum> {
-public:
-    static ProtoType encode(const LocalType& local){
-        switch (local) {
-            case SimpleEnum::Invalid:
-                return PROTO_SimpleEnum_Invalid;
-            case SimpleEnum::ValueOne:
-                return PROTO_SimpleEnum_ValueOne;
-            case SimpleEnum::ValueTwo:
-                return PROTO_SimpleEnum_ValueTwo;
-        }
-        return PROTO_SimpleEnum_Invalid;
-    };
-    static LocalType decode(const ProtoType& proto){
-        switch (proto) {
-            case PROTO_SimpleEnum_Invalid:
-                return SimpleEnum::Invalid;
-            case PROTO_SimpleEnum_ValueOne:
-                return SimpleEnum::ValueOne;
-            case PROTO_SimpleEnum_ValueTwo:
-                return SimpleEnum::ValueTwo;
-        }
-        return SimpleEnum::Invalid;
-    };
-};
 
 template <class CONTAINER>
 struct LOCAL_TestMessage {
